c633823a99e0b2fef8bd39bf5d728f23efec237f
duongdd141100 <duongdd141100@gmail.com>
2024-01-08 07:35:48 +0700

    Init

---
+++ src/main/java/com/example/cheduletaskdemo/CheduleTaskDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheduletaskdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheduleTaskDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheduleTaskDemoApplication.class, args);
+    }
+
+}
